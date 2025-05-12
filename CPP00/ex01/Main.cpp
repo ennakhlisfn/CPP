17786ae0f6f9fd9465afca826990e050ba5cb1d0
sennakhl <sennakhl@c3r2p3.1337.ma>
2025-05-12 20:29:41 +0100

    CPP00 done

--- CPP00/ex01/Main.cpp
+++ CPP00/ex01/Main.cpp
@@ -9,21 +9,36 @@ class Contact{
 		std::string	secret;
 };
 
+std::string	replaceTabsWithSpaces(std::string str){
+	size_t	pos = 0;
+	std::string	tab = "\t";
+	std::string	spaces = "    ";
+
+	while((pos = str.find(tab, pos)) != std::string::npos){
+		str.replace(pos, tab.size(), spaces);
+		pos += spaces.size();
+	}
+	return	str;
+}
+
 void	display_str(std::string	str)
 {
-	int	sz = str.size();
+	int	sz;
+	std::string NewStr;
 
+	NewStr = replaceTabsWithSpaces(str);
+	sz = NewStr.size();
 	std::cout << "  ";
 	if (sz > 10)
 	{
-		str.resize(9);
-		str.resize(10, '.');
+		NewStr.resize(9);
+		NewStr.resize(10, '.');
 	}
 	else if (sz < 10)
-		str.resize(sz+(10 - sz), ' ');
-	std::cout << str << "  |";
+		NewStr.resize(sz+(10 - sz), ' ');
+	std::cout << NewStr << "  |";
 }
-	
+
 
 class PhoneBook{
 	int	i = 0, k = 0;
@@ -31,50 +46,53 @@ class PhoneBook{
 
 	public:
 		std::string cmd;
-		void	add(){
-			std::cout << "\n\nfirst name: ";
-			std::cin >> contacts[i].fname;
-			if (std::cin.fail())
-				return ;
-			std::cout << "\nlast name: ";
-			std::cin >> contacts[i].lname;
-			if (std::cin.fail())
-				return ;
-			std::cout << "\nnickname name: ";
-			std::cin >> contacts[i].nname;
-			if (std::cin.fail())
-				return ;
-			std::cout << "\nphone number: ";
-			std::cin >> contacts[i].phone;
-			if (std::cin.fail())
-				return ;
-			std::cout << "\ndarkest secret: ";
-			std::cin >> contacts[i].secret;
-			if (std::cin.fail())
-				return ;
-			std::cout << "\n\n";
-			i++;
-			if (k < 8)
-				k++;
-			if (i == 8)
-				i = 0;
-		}
-
-		void search(){
-			std::cout << "  Index  |  First Name  |  Last Name   |  Nickname    |\n";
-			std::cout << "-------------------------------------------------------\n";
-			for (int j=0; j < k; j++)
-			{
-				std::cout << "    " << j << "    |";
-				display_str(contacts[j].fname);
-				display_str(contacts[j].lname);
-				display_str(contacts[j].nname);
-				std::cout << std::endl;
-			}
-			std::cout << std::endl;
-			std::cout << std::endl;
-		}
+		void	add();
+		void search();
 };
+
+void PhoneBook::add(){
+	std::cout << "\n\nfirst name: ";
+	std::getline(std::cin, contacts[i].fname);
+	if (std::cin.fail() || !contacts[i].fname.length())
+		return ;
+	std::cout << "\nlast name: ";
+	std::getline(std::cin, contacts[i].lname);
+	if (std::cin.fail() || !contacts[i].lname.length())
+		return ;
+	std::cout << "\nnickname name: ";
+	std::getline(std::cin, contacts[i].nname);
+	if (std::cin.fail() || !contacts[i].nname.length())
+		return ;
+	std::cout << "\nphone number: ";
+	std::getline(std::cin, contacts[i].phone);
+	if (std::cin.fail() || !contacts[i].phone.length())
+		return ;
+	std::cout << "\ndarkest secret: ";
+	std::getline(std::cin, contacts[i].secret);
+	if (std::cin.fail() || !contacts[i].secret.length())
+		return ;
+	std::cout << "\n\n";
+	i++;
+	if (k < 8)
+		k++;
+	if (i == 8)
+		i = 0;
+}
+
+void	PhoneBook::search(){
+	std::cout << "  Index  |  First Name  |  Last Name   |  Nickname    |\n";
+	std::cout << "-------------------------------------------------------\n";
+	for (int j=0; j < k; j++)
+	{
+		std::cout << "    " << j << "    |";
+		display_str(contacts[j].fname);
+		display_str(contacts[j].lname);
+		display_str(contacts[j].nname);
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
 
 int main() {
 	PhoneBook	phonebook;
@@ -86,12 +104,12 @@ int main() {
 		std::cout << "\tSEARCH : to display a specific phonebookact\n";
 		std::cout << "\tEXIT : to quits\n\n";
 		std::cout << "command : ";
-		std::cin >> phonebook.cmd;
+		std::getline(std::cin, phonebook.cmd);
 		if (std::cin.fail()) break ;
 		if (phonebook.cmd.compare("EXIT") == 0) break ;
 		else if (phonebook.cmd.compare("ADD") == 0) phonebook.add();
 		else if (phonebook.cmd.compare("SEARCH") == 0) phonebook.search();
-		if (std::cin.fail()) break ;
+		if (std::cin.fail()) break;
 	}
   return 0;
 }
--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,9 +1,36 @@
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
+
+void  Account::_displayTimestamp(){
+  std::time_t now = std::time(NULL);
+  std::tm*    timeInfo = std::localtime(&now);
+
+  std::cout << "["
+            << std::setfill('0')
+            << std::setw(4) << (timeInfo->tm_year + 1900)
+            << std::setw(2) << (timeInfo->tm_mon + 1)
+            << std::setw(2) << timeInfo->tm_mday << "_"
+            << std::setw(2) << timeInfo->tm_hour
+            << std::setw(2) << timeInfo->tm_min
+            << std::setw(2) << timeInfo->tm_sec
+            << "] ";
+}
+
+Account::Account(int initial_deposit){
+  _amount = initial_deposit;
+  _nbAccounts++;
+  _totalAmount += initial_deposit;
+  _accountIndex = _nbAccounts - 1;
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created\n";
+}
 
 int Account::getNbAccounts(){
   return _nbAccounts;
@@ -22,8 +49,40 @@ int Account::getNbWithdrawals(){
 }
 
 void Account::displayAccountsInfos(){
-  getNbAccounts();
-  getTotalAmount();
-  getNbDeposits();
-  getNbWithdrawals();
+  _displayTimestamp();
+  std::cout << "accoutns:"<< getNbAccounts() << ";total:"<< getTotalAmount() <<";depostits:" << 
+  getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void  Account::makeDeposit(int deposit) {
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";p_amount:" << _amount 
+  << ";deposit:" << deposit << ";amount:" << _amount + deposit 
+  << ";nb_depostits:" << ++_nbDeposits << std::endl;
+  _amount += deposit;
+}
+
+bool  Account::makeWithdrawal(int withdrawal){
+  _displayTimestamp();
+  if (_amount - withdrawal < 0){
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount 
+    << ";withdrawal:refused" << std::endl;
+    return false;
+  }
+  std::cout << "index:" << _accountIndex << ";p_amount:" << _amount 
+  << ";withdrawal:" << withdrawal << ";amount:" << _amount - withdrawal
+  << ";nb_withdrawals:" << ++_nbWithdrawals << std::endl;
+  _amount -= withdrawal;
+  return true;
+}
+
+void  Account::displayStatus() const {
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" <<
+  _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+Account::~Account(){
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed\n";
 }
